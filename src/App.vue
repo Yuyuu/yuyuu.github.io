623a9f3ecffd3cd7d03434143963a4ab762a752f
vtertre <vincent.tertre@devoteamgcloud.com>
2018-08-23 12:34:46 +0200

    Add social links

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,19 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  margin: 40px auto;
+  max-width: 960px;
+}
+
+a {
+  color: inherit;
+  &:visited {
+    color: inherit;
+  }
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-container fluid="">
+  <v-container fluid="" class="py-0">
       <v-flex xs12="">
         <v-layout row="" align-center="" justify-start="" fill-height="">
           <v-avatar size="72" class="elevation-1">
             <img src="../assets/avatar.jpg" alt="avatar">
           </v-avatar>
-          <h1 class="ml-3">Vincent Tertre</h1>
+          <v-container fluid="" class="py-0">
+            <v-flex xs12="">
+              <v-layout column="" align-start="" justify-start="" fill-height="">
+                <h1>Vincent Tertre</h1>
+                <nav>
+                  <a href="https://github.com/vtertre" title="GitHub account">GitHub</a> |
+                  <a href="https://linkedin.com/in/vincenttertre" title="LinkedIn account">LinkedIn</a>
+                </nav>
+              </v-layout>
+            </v-flex>
+          </v-container>
         </v-layout>
       </v-flex>
     </v-container>
