1cd1b1fcf5b47f3dbe9bf2b8a58d0ad4ed98ac9b
vtertre <vincent.tertre@devoteamgcloud.com>
2018-11-01 16:26:11 +0100

    Go naked

---
+++ public/app.js
@@ -0,0 +1,52 @@
+window.onload = function () {
+  var activeTabs = document.getElementsByClassName('tab active');
+  if (activeTabs.length > 0) {
+    var targetElementId = activeTabs[0].dataset.target;
+    displayElementById(targetElementId);
+  }
+}
+
+function openTargetTab(event) {
+  var clickedTabElement = event.currentTarget;
+  var tabContentElementId = clickedTabElement.dataset.target;
+
+  deactivateTabs();
+  hideContent();
+  displayElementById(tabContentElementId);
+  activate(clickedTabElement);
+}
+
+function activate(tabElement) {
+  tabElement.classList.add('active');
+}
+
+function deactivate(tabElement) {
+  tabElement.classList.remove('active');
+}
+
+function deactivateTabs() {
+  var tabElements = document.getElementsByClassName('tab');
+  for (var i = 0; i < tabElements.length; ++i) {
+    deactivate(tabElements[i]);
+  }
+}
+
+function display(element) {
+  element.style.display = 'block';
+}
+
+function displayElementById(id) {
+  var element = document.getElementById(id);
+  display(element);
+}
+
+function hide(element) {
+  element.style.display = 'none';
+}
+
+function hideContent() {
+  var tabContentElements = document.getElementsByClassName('tab__content');
+  for (var i = 0; i < tabContentElements.length; ++i) {
+    hide(tabContentElements[i]);
+  }
+}
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  assetsDir: 'assets',
-  pwa: {
-    name: 'Vincent Tertre',
-    themeColor: '#4CAF50',
-    msTileColor: '#4CAF50'
-  }
-};
